0dc61d3fae1b3e74f6455ff4badd91fd72cd6bde
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-04 05:07:38 +0600

    home section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './Home/Home';
 import Header from './Header/Header';
 import { Route, Routes } from 'react-router-dom';
+import Reviews from './Reviews/Reviews';
+import Dashboard from './Dashboard/Dashboard';
+import Blogs from './Blogs/Blogs';
+import About from './About/About';
 
 function App() {
   return (
@@ -11,7 +15,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/reviews' ></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import './Home.css';
+import img1 from '../images/lamp8.PNG';
 const Home = () => {
+
     return (
         <div>
-        <h1>This is Home</h1>    
+            <div className='home-main'>
+            <div className='home-details text-orange-500  text-left ml-9 m-9'>
+                <h1 className='text-6xl m-8'><span className='text-gray-600 '>PREMIUM LAMP</span> ZONE</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur,
+                    assumenda. Quos, veniam facere voluptatem quasi assumenda neque
+                    corrupti amet voluptate beatae recusandae suscipit quam commodi
+                    excepturi libero dolorum autem voluptates minima ullam aut ea
+                    fugiat laborum obcaecati. Cupiditate expedita perspiciatis
+                    tempora eligendi modi suscipit facilis blanditiis,
+                    ipsa nemo quo. Vel!</p>
+                    <button>Find Your Lamps</button>
+            </div>
+            <div className='home-img'>
+                <img src={img1} alt="" />
+            </div>
+            </div>
         </div>
     );
 };
 
-export default Home;<h1>This is Home</h1>
+export default Home; <h1>This is Home</h1>
