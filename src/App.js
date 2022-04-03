be93b68ab81c13488560cf55a1d0200fc5c78cdf
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-04-04 03:15:34 +0600

    added path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home';
+import Header from './Header/Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+           <Link to="/" className='header-link'>HOME</Link>
+           <Link to="/reviews" className='header-link' >REVIEWS</Link>
+           <Link to="/dashboard" className='header-link' >DASHBOARD</Link>
+           <Link to="/blogs" className='header-link' >BLOGS</Link>
+           <Link to="/about" className='header-link'>ABOUT</Link>
+        </div>
+    );
+};
+
+export default Header;<h1>This is Header</h1>
